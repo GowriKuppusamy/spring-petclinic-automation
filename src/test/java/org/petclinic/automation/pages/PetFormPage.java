7c119f4aade602b5571799593187dfa2f830d523
GowriKuppusamy <gowri1020k@gmail.com>
2026-07-27 19:19:38 +0530

    feat: add PetFormPage

---
+++ src/test/java/org/petclinic/automation/pages/PetFormPage.java
@@ -0,0 +1,39 @@
+package org.petclinic.automation.pages;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+
+public class PetFormPage extends BasePage {
+  private final Locator name;
+  private final Locator birthDate;
+  private final Locator typeSelect;
+  private final Locator savePetButton;
+
+  public PetFormPage(Page page) {
+    super(page);
+    this.name = page.locator("input#name");
+    this.birthDate = page.locator("input#birthDate");
+    this.typeSelect = page.locator("select#type");
+    this.savePetButton = page.getByRole().button(new Page.GetByRoleOptions().setName("Save Pet"));
+  }
+
+  public PetFormPage fillPet(string petName, String bornDate, String type) {
+    name.fill(petName);
+    birthDate.fill(bornDate);
+    typeSelect.selectOption(type);
+    return this;
+  }
+
+  public OwnerDetailsPage save() {
+    savePetButton.click();
+    return new OwnerDetailsPage(page);
+  }
+
+  public boolean hasFieldErrors() {
+    return page.locator(".text-danger").count() > 0;
+  }
+
+  public boolean hasDuplicateNameError() {
+    return page.locator("span:has-text('duplicate')").count() > 0;
+  }
+}
